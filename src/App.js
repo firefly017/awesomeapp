b112639378f2f23bfb5cf450ae8dd1eb118c1adc
firefly017 <jui.n.nahar@gmail.com>
2020-04-28 15:43:35 +0200

    some changes

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React!
+          Welcome home, Jui!
         </a>
       </header>
     </div>
